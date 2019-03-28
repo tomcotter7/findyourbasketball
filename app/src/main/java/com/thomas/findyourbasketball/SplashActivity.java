82429ce2c8c75da93d81aa4598ac48937b74e03f
tomcotter7 <thomascottter00@gmail.com>
2019-03-28 09:39:48 +0000

    comments

--- app/src/main/java/com/thomas/findyourbasketball/SplashActivity.java
+++ app/src/main/java/com/thomas/findyourbasketball/SplashActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    //This method displays my loading screen for four seconds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +15,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                //This intent starts the mainActivity after four seconds of displaying the loading screen.
+                //This intent provides the data - the MainActivity class.
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                //This starts the activity from the intent.
                 startActivity(mainIntent);
                 finish();
             }
